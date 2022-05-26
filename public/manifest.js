fb623c371b460e69a651f02b0abb144779f4f2b9
utilrr <gmarkghealy@gmail.com>
2022-05-26 17:46:08 -0400

    service worker.js

---
+++ public/manifest.js
@@ -0,0 +1,11 @@
+{
+    "name": "Budget Tracker",
+    "short_name": "Foodies",
+    "description": "An app that allows you to view upcoming food events.",
+  
+  "background_color": "#01579b",
+"theme_color": "#ffffff",
+
+"start_url": "/",
+"display": "standalone"
+}
